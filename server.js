275997462a1b45747d151f45e417ee9f1044f876
pablos <pablo.schrut@ngahr.com>
2019-05-19 19:28:39 +0200

    Primer commit

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+
+const hbs = require('hbs');
+require('./hbs/helpers');
+const port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+app.get('/', (req, res) => {
+	res.render('home', {
+		nombre: 'pablo',
+		anio: new Date().getFullYear()
+	});
+});
+
+app.get('/about', (req, res) => {
+	res.render('about', {
+		anio: new Date().getFullYear()
+	});
+});
+
+app.listen(port, () => {
+	console.log(`Escuchando peticiones en el puerto ${port}`);
+});
